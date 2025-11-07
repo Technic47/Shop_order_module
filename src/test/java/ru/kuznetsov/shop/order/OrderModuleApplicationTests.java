21dd3c70d927e3d356fe4550e7c285b9540072e2
Pavel Kuznetsov <Technic47@gmail.com>
2025-11-07 23:06:23 +0300

    Init commit.

---
+++ src/test/java/ru/kuznetsov/shop/order/OrderModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuznetsov.shop.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
